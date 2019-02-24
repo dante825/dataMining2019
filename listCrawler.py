a4b5da9d2ddfa69e247f12f69a13e9880a324cb4
Dante <kangwei9125@gmail.com>
2019-02-24 20:44:35 +0800

    Able to scrap both links and symbol now. DB inserting is next

--- listCrawler.py
+++ listCrawler.py
@@ -20,82 +20,32 @@ class Render(QWebPage):
         self.app.quit()
 
 url = 'https://www.thestar.com.my/business/marketwatch/stock-list/?sector=main_healthcare'
+# url = 'https://www.thestar.com.my/business/marketwatch/stock-list/?sector=main_finance'
 # This does the magic. Loads everything
 r = Render(url)
-# result if a QString
+# result is a QString
 result = r.frame.toHtml()
 
 # QString should be converted to string before processed by lxml
 formatted_result = str(result.encode('UTF-8'))
 
+# print(formatted_result)
+
 # Next build lxml tree from formatted result
 tree = html.fromstring(formatted_result)
 
 # Now using correct xpath we are fetching the URLs of the archive
-archive_links = tree.xpath('//tbody/tr[@class="linedlist"]/td//text()')
-print(archive_links)
+rawStocks = tree.xpath('//tbody/tr[@class="linedlist"]/td//text()')
+rawStocks = rawStocks[:rawStocks.index('CAB')]
+rawStocks = rawStocks[:int(len(rawStocks) / 2)]
 
-# ------------------------------------
-# class AppCrawler:
-#     def __init__(self, starting_url, depth):
-#         self.starting_url = starting_url
-#         self.depth = depth
-#         self.apps = []
-#
-#     def crawl(self):
-#         self.get_app_from_link(self.starting_url)
-#         return
-#
-#     def get_app_from_link(self, link):
-#         start_page = requests.get(link)
-#         tree = html.fromstring(start_page.text)
-#         print(start_page.text)
-#
-#         table = tree.xpath('//tbody/tr[@class="linedlist"]/td//text()')
-#         # name = tree.xpath('//h1[@class="stock-profile f16"]/text()')[0]
-#         # open = tree.xpath('//td[@id="slcontent_0_ileft_0_opentext"]/text()')[0]
-#         # high = tree.xpath('//td[@id="slcontent_0_ileft_0_hightext"]/text()')[0]
-#         # low = tree.xpath('//td[@id="slcontent_0_ileft_0_lowtext"]/text()')[0]
-#         # close = tree.xpath('//td[@id="slcontent_0_ileft_0_lastdonetext"]/text()')[0]
-#         # code = tree.xpath('//li[@class="f14"]/text()')[1]
-#
-#         # print(name)
-#         # print(open)
-#         # print(high)
-#         # print(low)
-#         # print(close)
-#         # print(code[3:])
-#         print(table)
-#
-#         return
-#
-#
-# class App:
-#
-#     def __init__(self, shortName, link, table):
-#         # self.name = name
-#         # self.code = code
-#         # self.open = open
-#         # self.high = high
-#         # self.low = low
-#         # self.close = close
-#         # self.links = links
-#         self.table = table
-#
-#     def __str__(self):
-#         # return ("tbody: " + self.root.encode('UTF-8') + "\r\n")
-#         return table
-#         # return ("Name: " + self.name.encode('UTF-8') +
-#         #         "\r\nCode: " + self.developer.encode('UTF-8') +
-#         #         "\r\nOpen: " + self.open.encode('UTF-8') +
-#         #         "\r\nHigh: " + self.high.encode('UTF-8') +
-#         #         "\r\nLow: " + self.low.encode('UTF-8') +
-#         #         "\r\nClose: " + self.close.encode('UTF-8') + "\r\n")
-#
-#
-# crawler = AppCrawler("https://www.thestar.com.my/business/marketwatch/stock-list/?sector=main_healthcare", 0)
-#
-# crawler.crawl()
-#
-# for app in crawler.apps:
-#     print(app)
+rawLinks = tree.xpath('//tr[@class="linedlist"]/td/a/@href')
+
+cleanStocks = []
+for i in range(0, len(rawStocks)):
+    if i % 8 == 0:
+        cleanStocks.append(rawStocks[i])
+
+cleanLinks = rawLinks[:len(cleanStocks)]
+print(cleanStocks)
+print(cleanLinks)
