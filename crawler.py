84195c65eb59d2ac442184e280da18330aab64f2
Dante <kangwei9125@gmail.com>
2019-03-04 11:35:02 +0800

    Added exception handling to crawler and new backup on db

--- crawler.py
+++ crawler.py
@@ -15,24 +15,27 @@ class AppCrawler:
     def crawl(self, url_param):
         return self.get_app_from_link(url_param)
 
-    def get_app_from_link(self, link):
+	def get_app_from_link(self, link):
         start_page = requests.get(link)
         tree = html.fromstring(start_page.text)
 
-        date = tree.xpath('//*[@id="slcontent_0_ileft_0_datetxt"]/text()')[0]
-        time = tree.xpath('//*[@id="slcontent_0_ileft_0_timetxt"]/text()')[0]
-        name = tree.xpath('//h1[@class="stock-profile f16"]/text()')[0]
-        open_price = tree.xpath('//td[@id="slcontent_0_ileft_0_opentext"]/text()')[0]
-        high = tree.xpath('//td[@id="slcontent_0_ileft_0_hightext"]/text()')[0]
-        low = tree.xpath('//td[@id="slcontent_0_ileft_0_lowtext"]/text()')[0]
-        close = tree.xpath('//td[@id="slcontent_0_ileft_0_lastdonetext"]/text()')[0]
-        code = tree.xpath('//li[@class="f14"]/text()')[1]
-        vol = tree.xpath('//*[@id="slcontent_0_ileft_0_voltext"]/text()')[0]
-        buy_vol = tree.xpath('//*[@id="slcontent_0_ileft_0_buyvol"]/text()')[0]
-        sell_vol = tree.xpath('//*[@id="slcontent_0_ileft_0_sellvol"]/text()')[0]
+        try:
+            date = tree.xpath('//*[@id="slcontent_0_ileft_0_datetxt"]/text()')[0]
+            time = tree.xpath('//*[@id="slcontent_0_ileft_0_timetxt"]/text()')[0]
+            name = tree.xpath('//h1[@class="stock-profile f16"]/text()')[0]
+            open_price = tree.xpath('//td[@id="slcontent_0_ileft_0_opentext"]/text()')[0]
+            high = tree.xpath('//td[@id="slcontent_0_ileft_0_hightext"]/text()')[0]
+            low = tree.xpath('//td[@id="slcontent_0_ileft_0_lowtext"]/text()')[0]
+            close = tree.xpath('//td[@id="slcontent_0_ileft_0_lastdonetext"]/text()')[0]
+            code = tree.xpath('//li[@class="f14"]/text()')[1]
+            vol = tree.xpath('//*[@id="slcontent_0_ileft_0_voltext"]/text()')[0]
+            buy_vol = tree.xpath('//*[@id="slcontent_0_ileft_0_buyvol"]/text()')[0]
+            sell_vol = tree.xpath('//*[@id="slcontent_0_ileft_0_sellvol"]/text()')[0]
 
-        stock = Stock(date[10: -2], time, name, code[3:], open_price, high, low, close, vol, buy_vol, sell_vol)
-        return stock
+            stock = Stock(date[10: -2], time, name, code[3:], open_price, high, low, close, vol, buy_vol, sell_vol)
+            return stock
+        except IndexError:
+            return None
 
 
 class Stock:
@@ -73,9 +76,12 @@ def main():
     for details in stocks_sym:
         s = crawler.crawl(details[1])
         logging.info('Scraping %s', details[1])
-        db_op.insert_stocks(s.date, s.time, s.name, s.code, clean_dash_num(s.open), clean_dash_num(s.high), clean_dash_num(s.low),
-                            clean_dash_num(s.close), s.vol, s.buy_vol, s.sell_vol)
-    db_op.close_conn()
+        if s is None:
+            logging.info('Link Invalid. Skipped.')
+        else:
+            db_op.insert_stocks(s.date, s.time, s.name, s.code, clean_dash_num(s.open), clean_dash_num(s.high),
+                                clean_dash_num(s.low), clean_dash_num(s.close), s.vol, s.buy_vol, s.sell_vol)
+    db_op.close_conn()    
 
 
 if __name__ == '__main__':
