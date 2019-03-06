f08ae4f482d62426668db0d15614b4c0f70b2e4e
Dante <kangwei9125@gmail.com>
2019-03-06 22:55:37 +0800

    Change the database to PostgreSQL

--- DbOperations.py
+++ DbOperations.py
@@ -1,40 +1,64 @@
-import sqlite3
+import psycopg2
+from configparser import ConfigParser
+import locale
+import logging
 
-INSERT_LINK_SQL = '''INSERT INTO Stocks_link (symbol, link, sector) VALUES (?,?,?);'''
-INSERT_STOCK = '''INSERT INTO Stocks(date, time, stock_name, stock_code, open, high, low, close, vol, buy_vol, sell_vol)
-                VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+# Setup the log level
+logging.basicConfig(level=logging.INFO)
+
+locale.setlocale( locale.LC_ALL, 'en_US.UTF-8' )
+
+INSERT_LINK_SQL = '''INSERT INTO Stocks_link (symbol, link, sector) VALUES (%s,%s,%s);'''
+INSERT_STOCK = '''INSERT INTO stocks (date, time, stock_code, stock_name, open, high, low, close, vol, buy_vol, 
+        sell_vol) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) '''
 VIEW_STOCKS_LINK = '''SELECT * FROM Stocks_link;'''
 VIEW_STOCKS = '''SELECT * FROM Stocks;'''
-TRUNCATE_STOCKS_LINK = '''DELETE FROM Stocks_link;'''
-TRUNCATE_STOCKS = '''DELETE FROM Stocks;'''
-DB_LOCATION = 'E:\\development\\repo\\icedTeaSpider\\stockDb.sqlite'
-DROP_STOCKS_LINK_TABLE = '''DROP TABLE IF EXISTS Stocks_link;'''
-DROP_STOCKS_TABLE = '''DROP TABLE IF EXISTS Stocks;'''
-CREATE_STOCKS_LINK_SQL = '''CREATE TABLE Stocks_link (
+TRUNCATE_STOCKS_LINK = '''TRUNCATE TABLE stocks_link;'''
+TRUNCATE_STOCKS = '''TRUNCATE TABLE stocks;'''
+DROP_STOCKS_LINK_TABLE = '''DROP TABLE IF EXISTS stocks_link;'''
+DROP_STOCKS_TABLE = '''DROP TABLE IF EXISTS stocks;'''
+CREATE_STOCKS_LINK_SQL = '''CREATE TABLE stocks_link (
     symbol TEXT NOT NULL PRIMARY KEY,
     link TEXT,
     sector TEXT
     );
 '''
-CREATE_STOCKS_TABLE = '''CREATE TABLE Stocks (
-    date TEXT,
-    time TEXT,
+CREATE_STOCKS_TABLE = '''CREATE TABLE stocks (
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    stock_code TEXT NOT NULL,
     stock_name TEXT,
-    stock_code TEXT,
-    open NUMERIC,
-    high NUMERIC,
-    low NUMERIC,
-    close NUMERIC,
-    vol NUMERIC,
+    open FLOAT(3),
+    high FLOAT(3),
+    low FLOAT(3),
+    close FLOAT(3),
+    vol INTEGER,
     buy_vol TEXT,
-    sell_vol TEXT
+    sell_vol TEXT,
+    PRIMARY KEY (date, time, stock_code)
     );    
 '''
 
 
 class DbOperations:
-    def __init__(self):
-        self.conn = sqlite3.connect(DB_LOCATION)
+    def __init__(self, configfile='database.ini', section='postgresql'):
+        # Create a parser
+        parser = ConfigParser()
+        # Read the config file
+        parser.read(configfile)
+
+        # Get section, default to postgresql
+        db = {}
+        if parser.has_section(section):
+            logging.info('Reading database config file')
+            params = parser.items(section)
+            for param in params:
+                db[param[0]] = param[1]
+        else:
+            logging.error('Section {0} not found in {1} file'. format(section, configfile))
+
+        logging.info('Connecting to database...')
+        self.conn = psycopg2.connect(**db)
 
     def create_table(self, drop_script, create_script):
         """
@@ -58,7 +82,7 @@ class DbOperations:
         cur = self.conn.cursor()
         cur.execute(INSERT_LINK_SQL, (symbol, link, sector))
 
-    def insert_stocks(self, date, time, name, code, open, high, low, close, vol, buy_vol, sell_vol):
+    def insert_stocks(self, date, time, code, name, open, high, low, close, vol, buy_vol, sell_vol):
         """
         Inserting a stock details into stock table
         :param date: date last updated
@@ -75,7 +99,12 @@ class DbOperations:
         :return: void
         """
         cur = self.conn.cursor()
-        cur.execute(INSERT_STOCK, (date, time, name, code, open, high, low, close, vol, buy_vol, sell_vol))
+        try:
+            cur.execute(INSERT_STOCK, (date, time, code, name, open, high, low, close, clean_volume(vol), buy_vol, sell_vol))
+            self.conn.commit()
+        except psycopg2.IntegrityError as e:
+            logging.error('Record {0}, {1}, {2}, {3} already exists.'.format(date, time, code, name))
+            self.conn.rollback()
 
     def view_table(self, select_script):
         """
@@ -102,25 +131,40 @@ class DbOperations:
         Commit and close the database connection
         :return: void
         """
+        logging.info('Committing changes and close database connection.')
         self.conn.commit()
         self.conn.close()
 
 
+def clean_volume(numstr):
+    if type(numstr) is int:
+        return numstr
+    else:
+        return locale.atoi(numstr)
+
+
 def main():
     db_op = DbOperations()
-    # db_op.insert_symlink('sapura', 'star/business/sapura', 'sector')
-    # db_op.create_table(DROP_TABLE, CREATE_STOCKS_LINK_SQL)
-    # records = db_op.view_table(VIEW_STOCKS_LINK)
-
+    # Create tables
+    # db_op.create_table(DROP_STOCKS_LINK_TABLE, CREATE_STOCKS_LINK_SQL)
     # db_op.create_table(DROP_STOCKS_TABLE, CREATE_STOCKS_TABLE)
-    # db_op.insert_stocks('2019-02-10', '7.00', 'sapura holdings', '5211', 10.0, 1.20, 1.01, 1.15, 5200, '5300', '5866')
-    # db_op.clear_table(TRUNCATE_STOCKS)
-    records = db_op.view_table(VIEW_STOCKS)
-    db_op.close_conn()
 
+    # Record inserting test
+    # db_op.insert_symlink('sapura2', 'star/business/sapura', 'sector')
+    # db_op.insert_stocks('28 Feb 2019', '5:00 pm', '5211', 'sapura holdings', 10.0, 1.20, 1.01, 1.15, 12500,
+    #                     '0.101 / 125', '0.111 / 153')
+
+    # Truncate the table
+    # db_op.clear_table(TRUNCATE_STOCKS_LINK)
+    # db_op.clear_table(TRUNCATE_STOCKS)
+
+    # Viewing the records in the table
+    records = db_op.view_table(VIEW_STOCKS)
+    # records = db_op.view_table(VIEW_STOCKS_LINK)
     print(len(records))
     for record in records:
         print(record)
+    db_op.close_conn()
 
 
 if __name__ == '__main__':
